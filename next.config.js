0d846eab632910023db847d4f10015f8cb03eb42
Ivaη Espínola G <ivan.espinola@live.com>
2020-11-09 23:50:49 -0300

    Feat: i18n settings

--- next.config.js
+++ next.config.js
@@ -13,6 +13,10 @@ module.exports = {
     autoPrerender: true,
   },
   env: {},
+  i18n: {
+    locales: ['pt'],
+    defaultLocale: 'pt-BR',
+  },
   redirects: async () => [
     {
       source: '/',
